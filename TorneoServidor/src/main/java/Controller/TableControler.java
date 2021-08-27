41083f376fbe976ee43c45a9568619e985b3068d
Luis <cookie30.ld@gmail.com>
2021-08-27 16:37:34 -0500

    Ordenando el codigo del servidor para implementar
    Se organiza la arquitectura para luego añadir la logica.

--- TorneoServidor/src/main/java/Controller/TableControler.java
+++ TorneoServidor/src/main/java/Controller/TableControler.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.grupo4.torneoservidor.Model.DTOs;
+package Controller;
 
 /**
  *
  * @author Luis
  */
-public class DtoExample {
+public class TableControler {
     
 }
---
+++ TorneoServidor/src/main/java/Services/Services/ServiceCompetitor.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services.Services;
+
+
+import Models.Competitor;
+import Services.Interfaces.IServiceCompetitor;
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Luis
+ */
+public class ServiceCompetitor implements IServiceCompetitor{
+
+    @Override
+    public Competitor searchCompetitorById(long id) {
+        
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateCompetitor(Competitor competitor) {
+        
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteCompetitor(long id) {
+        
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createCompetitor(Competitor competitor) {
+        
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    
+    
+    
+}
