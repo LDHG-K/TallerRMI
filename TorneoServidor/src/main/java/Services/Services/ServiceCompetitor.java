3fd8ad9d612f404bd40751da059e8bf1974072af
Luis <cookie30.ld@gmail.com>
2021-08-28 18:36:24 -0500

    Implementando el metodo de actualizar en el servicio participante

--- TorneoServidor/src/main/java/Services/Services/ServiceCompetitor.java
+++ TorneoServidor/src/main/java/Services/Services/ServiceCompetitor.java
@@ -56,8 +56,19 @@ public class ServiceCompetitor  extends UnicastRemoteObject implements IServiceC
     @Override
     public void updateCompetitor(Competitor competitor)throws RemoteException {
         
+        String cad = "UPDATE Participantes SET apodo ="+competitor.getApodo()+
+                "fecha_inscripcion =" + competitor.getFechaInscripcion()+
+                "fecha_caducidad ="+competitor.getFechaCaducidad()+
+                "WHERE id = "+competitor.getId();
+        try {
+            if (!connection.executeUpdateStatement(cad)) {
+                throw new Exception("Operacion no ejecutada");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
         
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
