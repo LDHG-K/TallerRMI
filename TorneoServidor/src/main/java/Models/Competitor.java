2b85ff9feda68a9cc10ad52d41705b630a4f1cb9
luislftv <lftv960708@gmail.com>
2021-08-27 22:07:51 -0500

    Clase estructural competitor editada

--- TorneoServidor/src/main/java/Models/Competitor.java
+++ TorneoServidor/src/main/java/Models/Competitor.java
@@ -5,10 +5,58 @@
  */
 package Models;
 
+import java.util.Date;
+
 /**
  *
  * @author Luis
  */
 public class Competitor {
+
+    private int id;
+    private String apodo;
+    private Date fechaInscripcion;
+    private Date fechaCaducidad;
+
+    public Competitor(int id, String apodo, Date fechaInscripcion, Date fechaCaducidad) {
+        this.id = id;
+        this.apodo = apodo;
+        this.fechaInscripcion = fechaInscripcion;
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public Date getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+
+    public void setFechaInscripcion(Date fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public Date getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(Date fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    
     
 }
