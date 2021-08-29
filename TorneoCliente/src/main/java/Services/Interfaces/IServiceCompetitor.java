f5e9f1d0ebc3975ee563e016029fcf88173acf55
luislftv <lftv960708@gmail.com>
2021-08-29 11:15:12 -0500

    Clases agregadas a TorneoClientes

---
+++ TorneoCliente/src/main/java/Services/Interfaces/IServiceCompetitor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services.Interfaces;
+
+import Models.Competitor;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ *
+ * @author Luis
+ */
+public interface IServiceCompetitor extends Remote {
+    
+    Competitor searchCompetitorById(long id) throws RemoteException;
+    
+    void updateCompetitor(Competitor competitor)throws RemoteException;
+    
+    void deleteCompetitor(long id)throws RemoteException;
+    
+    void createCompetitor(Competitor competitor)throws RemoteException;
+    
+    List<Competitor> searchAll()throws RemoteException;
+
+}
